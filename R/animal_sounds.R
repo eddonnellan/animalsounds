b2cc055b788d2ff7c7677e9fb0674d27cf5790ed
Donnellan <u2273254@live.warwick.ac.uk>
2025-06-23 13:37:07 +0100

    Updated links in documentation

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,9 +1,11 @@
 #' Print an animal's sounds and a random gentle insult
 #'
+#' Wraps [paste0()] and [sample()]. Use [set.seed()] for reproducible insults.
+#'
 #' @param animal A string representing an animal
 #' @param sound A string representing an animal sound
 #'
-#' @returns A string of the form "The (animal) goes (sound)! You (gentle insult)."
+#' @returns A string of the form "The \{animal\} goes \{sound\}! You \{gentle insult\}."
 #' @export
 #'
 #' @examples
