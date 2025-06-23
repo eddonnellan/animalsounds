a64181b96d27886bf1da6e42a2015943143de2d8
Donnellan <u2273254@live.warwick.ac.uk>
2025-06-23 13:29:06 +0100

    Document animal_sounds

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,5 +1,17 @@
+#' Print an animal's sounds and a random gentle insult
+#'
+#' @param animal A string representing an animal
+#' @param sound A string representing an animal sound
+#'
+#' @returns A string of the form "The (animal) goes (sound)! You (gentle insult)."
+#' @export
+#'
+#' @examples
+#' animal_sounds("dog", "woof")
+#' animal_sounds("chimp", "ugh ugh")
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
-  paste0("The ", animal, " goes ", sound, "! You muppet.")
+  insult<-sample(c("muppet", "numpty", "silly-billy"))[1]
+  paste0("The ", animal, " goes ", sound, "! You ", insult, ".")
 }
